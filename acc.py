d46462ad352b8e988cfb121ef9bd47ce167f1398
Erik Peterson <Erik.Exists@gmail.com>
2013-02-11 16:35:31 -0600

    Added process_prng to misc facilitate all acc.* functions now optionally taking a np.random.RandomState() object.  Passing of RandomStates allows for these functions to be correctly used in parallel runs, i.e.
    When random methods are used inside multiprocessing calls (like map()) the random seed is normally inherited from the parent.  This means that each parallel process is using the exact same random numbers, making each parallel process completely redundant.  To correct for this RandomState() is called inside the processes, and passed around between stochastic functions (in a process) to ensure each is using the right seed.

--- acc.py
+++ acc.py
@@ -1,70 +1,97 @@
 """Monte Carlo simulations of accuracy"""
 import numpy as np
 import scipy.stats as stats
-
-def random(N,p):
-	"""
-	Generate and returns a random sequence of {1,0} impulses of length N 
-	with p probability of drawing a 1.
-	"""
-
-	acc = np.random.binomial(1,p,(N))
-	return list(acc), [p]*N
+from simBehave.misc import process_prng
 
 
-def learn(N,loc):
-	"""
-	Generates and returns an binomial array of length N where the p(1) 
-	(also returned) increases with the CDF of the normal distribution, 
-	integrated from 0.01 to 10, plus white noise.
-	
-	The learning rate is determined by sampling of a normal distribution 
-	centered on loc.  
-	
-	Note: A loc of 3 gives curves qualitatively similar to learning 
-	patterns often observed in the abstract categorization tasks 
-	common to the Seger Lab.
-	"""
-
-	## Learn:
-	## Create a noisy range for the CDF,
-	trials = np.arange(.01,10,10/float(N))
-	trials = trials + stats.norm.rvs(size=trials.shape[0]) 
-	p_values = stats.norm.cdf(trials,loc)			
-
-	## And p_values becomes acc
-	acc = []
-	[acc.append(int(np.random.binomial(1,p,(1)))) for p in p_values] 
-
-	return acc, list(p_values)
+def random(N, p, prng=None):
+    """
+    Generate and returns a random sequence of {1,0} impulses of length <N> 
+    with p probability of drawing a 1.
+    """
+    
+    prng = process_prng(prng)
+    
+    acc = prng.binomial(1, p, N)
+    return acc.tolist(), [p, ] * N, prng
 
 
-def random_learn(N,p_rand,loc):
-	"""
-	Generates and returns an binomial array of length N where the p(1) 
-	(also returned) increases with the CDF of the normal distribution, 
-	after random trial T (sampled from a uniform distribution).  Before
-	T accuracy is random governed .
-	"""
-	from simBehave.acc import random
+def learn(N, loc, prng=None):
+    """
+    Generates and returns an binomial array of length N where the p(1) 
+    (also returned) increases with the CDF of the normal distribution, 
+    integrated from 0.01 to 10, plus white noise.
+    
+    The learning rate is determined by sampling of a normal distribution 
+    centered on loc.  
+    
+    Note: A loc of 3 gives curves qualitatively similar to learning 
+    patterns often observed in the abstract categorization tasks 
+    common to the Seger Lab.
+    """
 
-	T = int(np.random.randint(0,N,1))
-	
-	acc_1, p_1 = random(T,p_rand)
-	
-	# Learn:
-	trials = np.arange(.01,10,10/float(N-T))
-	trials = trials + stats.norm.rvs(size=trials.shape[0]) 
-	p_2 = stats.norm.cdf(trials,loc)			
-	p_2[p_2 < 0.5] = 0.5
-		## Rwmove p vals lees than 0.5 -- 
-		## we don't want below chance sampling
-		
-	acc_2 = []
-	[acc_2.append(int(np.random.binomial(1,p,(1)))) for p in p_2]
-	
-	acc = acc_1 + acc_2
-	p = list(p_1) + list(p_2)
+    prng = process_prng(prng)
+    
+    # Pass random state from prng to np so that
+    # stats.<> will inherit the irght state.
+    # There does not seem to be a way to set random
+    # state of stats.* functions directly.
+    np.random.set_state(prng.get_state())
+    
+    ## Learn:
+    ## Create a noisy range for the CDF,
+    trials = np.arange(.01,10,10/float(N))
+    trials = trials + stats.norm.rvs(size=trials.shape[0]) 
+    p_values = stats.norm.cdf(trials,loc)            
 
-	return acc, p
+    # And p_values becomes acc
+    prng.set_state(np.random.get_state())
+        ## Pass the seed stat from np back to
+        ## prng, then we can use prng again...
+    
+    acc = [int(prng.binomial(1, p, 1)) for p in p_values] 
+
+    return acc, list(p_values), prng
+
+
+def random_learn(N, p_rand, loc, prng=None):
+    """
+    Generates and returns an binomial array of length N where the p(1) 
+    (also returned) increases with the CDF of the normal distribution, 
+    after random trial T (sampled from a uniform distribution).  Before
+    T accuracy is random governed .
+    """
+    from simBehave.acc import random
+
+    prng = process_prng(prng)
+    
+    T = int(prng.randint(0,N,1))
+
+    acc_1, p_1, prng = random(T, p_rand, prng)
+    
+    # Learn:
+    trials = np.arange(.01, 10, (10/float(N - T)))
+    
+    # Pass random state from prng to np so that
+    # stats.<> will inherit the irght state.
+    # There does not seem to be a way to set random
+    # state of stats.* functions directly.
+    np.random.set_state(prng.get_state())
+    
+    trials = trials + stats.norm.rvs(size=trials.shape[0]) 
+    p_2 = stats.norm.cdf(trials,loc)
+    p_2[p_2 < 0.5] = 0.5
+        ## Rwmove p vals lees than 0.5 -- 
+        ## we don't want below chance sampling
+        
+    prng.set_state(np.random.get_state())
+        ## Pass the seed stat from np back to
+        ## prng, then we can use prng again...
+    
+    acc_2 = [int(prng.binomial(1, p, 1)) for p in p_2]
+    
+    acc = acc_1 + acc_2
+    p = list(p_1) + list(p_2)
+
+    return acc, p, prng
 
--- misc.py
+++ misc.py
@@ -1,4 +1,6 @@
 """Misc or helper functions for simBehave"""
+import numpy as np
+
 
 def map_to_last_intra(conditions,impulses,num_intra_event,terminal=True):
 	"""
@@ -26,3 +28,20 @@ def map_to_last_intra(conditions,impulses,num_intra_event,terminal=True):
 				intra_impulses.extend([0,]*(num_intra_event-1)+[imp,])
 
 	return intra_impulses
+
+
+def process_prng(prng):
+    """ See if <prng> is a numpy.random.RandomState object. 
+    If it is return it.  If not, try and create one by 
+    RandomState(<prng>), which will work of prng was 
+    None or a number. """
+
+    # See if prng is a RandomState (and so has
+    # a state to get) or create a RandomState
+    # overriding prng
+    try:
+        prng.get_state()
+    except AttributeError:
+        prng = np.random.RandomState(prng)
+        
+    return prng
