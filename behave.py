2c4fa14cac8a93f996715b225c33f2c2f2d99106
Erik Peterson <Erik.Exists@gmail.com>
2013-02-11 17:18:55 -0600

    Remaining stochastic functions now (optionally) take and (always) return np.random.RandomState() objects.

--- behave.py
+++ behave.py
@@ -1,130 +1,148 @@
 """ Uses trials and acc to simulate a behavioral experiment"""
-import simBehave
-
-def random(N,k,event=True):
-	"""
-	Returns a trialset matching N,k along with matching accuracy and
-	probability lists. If event is True event_random is used in place of 
-	random to create the trialset.	
-	"""
-	from copy import deepcopy
-
-	if event:
-		trialset = simBehave.trials.event_random(N,k,mult=1)
-	else:
-		trialset = simBehave.trials.random(N,k)
-	
-	N_c = deepcopy(N)
-	if event: N_c += 1
-	acc = [0]*(N_c*k)
-	p = [0]*(N_c*k)
-
-	names = list(set(trialset))
-	for n in names:
-		## Skip null trials
-		if (n is 0) | (n is '0'):
-			continue
-
-		## How many trials/condition?
-		acc_n = []; p_n = []
-		acc_n, p_n = simBehave.acc.random(k,1/N)
-		
-		for ii,t in enumerate(trialset):
-			if t == n:
-				acc[ii] = acc_n.pop(0)
-				p[ii] = p_n.pop(0)
-
-	return trialset, acc, p
+from simBehave.acc import random as randomacc, random_learn, learn
+from simBehave.trials import event_random as event_randomtrial
+from simBehave.trials import random as randomtrial
+from simBehave.misc import process_prng
 
 
-def learn(N,k,loc,event=True):
-	"""
-	Returns a trialset matching N,k along with matching accuracy and
-	probability lists. If event is True event_random is used in place of 
-	random to create the trialset.
-	"""
-	## For each condition name (n),
-	## create acc_n,p_n then map those
-	## into acc,p for each matching trial (t)
-	## in the trialset	
-	from copy import deepcopy
+def random(N, k, event=True, prng=None):
+    """
+    Returns a trialset matching N,k along with matching accuracy and
+    probability lists. If event is True event_random is used in place of 
+    random to create the trialset.    
+    """
+    from copy import deepcopy
 
-	if event:
-		trialset = simBehave.trials.event_random(N,k,mult=1)
-	else:
-		trialset = simBehave.trials.random(N,k)
-	
-	N_c = deepcopy(N)
-	if event: N_c += 1
-	acc = [0]*(N_c*k)
-	p = [0]*(N_c*k)
+    prng = process_prng(prng)
+    
+    if event:
+        trialset, prng = event_randomtrial(N, k, 1, prng)
+    else:
+        trialset, prng = randomtrial(N, k, prng)
+    
+    N_c = deepcopy(N)
+    if event: 
+        N_c += 1
+    
+    acc = [0, ] * (N_c*k)
+    p = [0, ] * (N_c*k)
 
-	names = list(set(trialset))
-	for n in names:
-		## Skip null trials
-		if (n is 0) | (n is '0'):
-			continue
+    names = list(set(trialset))
+    for n in names:
+        ## Skip null trials
+        if (n == 0) | (n == '0'):
+            continue
 
-		## How many trials/condition?
-		acc_n = []; p_n = []
-		acc_n, p_n = simBehave.acc.random_learn(k,1/N,loc)
-		
-		for ii,t in enumerate(trialset):
-			if t == n:
-				acc[ii] = acc_n.pop(0)
-				p[ii] = p_n.pop(0)
+        ## How many trials/condition?
+        acc_n = []
+        p_n = []
+        acc_n, p_n, prng = randomacc(k, 1/N, prng)
+        
+        for ii, t in enumerate(trialset):
+            if t == n:
+                acc[ii] = acc_n.pop(0)
+                p[ii] = p_n.pop(0)
 
-	return trialset, acc, p
+    return trialset, acc, p, prng
 
 
-def some_learn(N,k,N_learn,loc,event=True,rand_learn=True):
-	"""
-	Creates 'uneven' acc, and p value distributions for k trials in 
-	N conditions in the returned trialset where N_learn is the number 
-	of conditions that show learning (via sim_acc_learn()).  
-	
-	N minus N_learn condtions simulated data will be governed instead 
-	by sim_acc_rand. If event is True an event-related trialset is 
-	created.
-	"""
-	from copy import deepcopy
+def learn(N, k, loc, event=True, prng=None):
+    """
+    Returns a trialset matching N,k along with matching accuracy and
+    probability lists. If event is True event_random is used in place of 
+    random to create the trialset.
+    """
+    from copy import deepcopy
+    
+    prng = process_prng(prng)
+    
+    ## For each condition name (n),
+    ## create acc_n,p_n then map those
+    ## into acc,p for each matching trial (t)
+    ## in the trialset
+    if event:
+        trialset, prng = event_randomtrial(N, k, 1, prng)
+    else:
+        trialset, prng = randomtrial(N, k, prng)
+    
+    N_c = deepcopy(N)
+    if event: 
+        N_c += 1
+    
+    acc = [0, ] * (N_c*k)
+    p = [0, ] * (N_c*k)
 
-	if N == N_learn: raise ValueError, 'N_learn must be less than N.'
-	if N_learn <= 0: raise ValueError, 'N_learn must be 1 or greater.'
+    names = list(set(trialset))
+    for n in names:
+        ## Skip null trials
+        if (n is 0) | (n is '0'):
+            continue
 
-	if event:
-		trialset = simBehave.trials.event_random(N,k,mult=1)
-	else:
-		trialset = simBehave.trials.random(N,k)
+        ## How many trials/condition?
+        acc_n = []
+        p_n = []
+        acc_n, p_n, prng = random_learn(k, 1/N, loc, prng)
+        for ii, t in enumerate(trialset):
+            if t == n:
+                acc[ii] = acc_n.pop(0)
+                p[ii] = p_n.pop(0)
 
-	N_c = deepcopy(N)
-	if event: N_c += 1
-	acc = [0]*(N_c*k)
-	p = [0]*(N_c*k)
+    return trialset, acc, p, prng
 
-	names = list(set(trialset))
-	for ii,n in enumerate(names):
-		## Skip null trials
-		if (n is 0) | (n is '0'):
-			print('n was 0, skipping.')
-			continue
 
-		## How many trials/condition?
-		acc_n = []; p_n = []
+def some_learn(N, k, N_learn, loc, event=True, rand_learn=True, prng=None):
+    """
+    Creates 'uneven' acc, and p value distributions for k trials in 
+    N conditions in the returned trialset where N_learn is the number 
+    of conditions that show learning (via sim_acc_learn()).  
+    
+    N minus N_learn condtions simulated data will be governed instead 
+    by sim_acc_rand. If event is True an event-related trialset is 
+    created.
+    """
+    from copy import deepcopy
 
-		## How many trials/condition?
-		if ii <= N_learn: 
-			print('Learning in iteration {0}.'.format(ii))
-			if rand_learn:
-				acc_n, p_n = simBehave.acc.random_learn(k,1./N,loc)
-			else:
-				acc_n, p_n = simBehave.acc.learn(k,loc)
-		else:
-			acc_n, p_n = simBehave.acc.random(k,1./N)
+    prng = process_prng(prng)    
+    if N == N_learn:
+        raise ValueError('N_learn must be less than N.')
+    if N_learn <= 0:
+        raise ValueError('N_learn must be 1 or greater.')
 
-		for jj,t in enumerate(trialset):
-			if t == n:
-				acc[jj] = acc_n.pop(0)
-				p[jj] = p_n.pop(0)
+    if event:
+        trialset, prng = event_randomtrial(N, k, mult=1, prng=prng)
+    else:
+        trialset, prng = randomtrial(N, k, prng)
 
-	return trialset, acc, p
+    N_c = deepcopy(N)
+    if event:
+        N_c += 1
+    
+    acc = [0, ] * (N_c*k)
+    p = [0, ] * (N_c*k)
+
+    names = list(set(trialset))
+    for ii,n in enumerate(names):
+        ## Skip null trials
+        if (n == 0) | (n == '0'):
+            continue
+
+        ## How many trials/condition?
+        acc_n = []
+        p_n = []
+
+        ## How many trials/condition?
+        if ii <= N_learn: 
+            print('Learning in iteration {0}.'.format(ii))
+            if rand_learn:
+                acc_n, p_n, prng = random_learn(k, 1./N, loc, prng)
+            else:
+                acc_n, p_n, prng = learn(k, loc, prng)
+        else:
+            acc_n, p_n, prng = randomacc(k,1./N)
+
+        for jj,t in enumerate(trialset):
+            if t == n:
+                acc[jj] = acc_n.pop(0)
+                p[jj] = p_n.pop(0)
+
+    return trialset, acc, p, prng
--- trials.py
+++ trials.py
@@ -1,85 +1,90 @@
 """Create lists of randomized trials."""
-## TESTED
 import os
 import numpy as np
-
-def random(N,k):
-	"""
-	Creates a trialset of N randomized conditions of total length N+1*k, 
-	where k is the number of trials/condition. Returns a list of conditions.
-	Conditions are indexed from 1; 0 implies terminal condtions, somthing 
-	this function can't handle.
-	"""
-
-	conditions = []
-	[conditions.extend([n]*k) for n in range(1,N+1)]	
-	np.random.shuffle(conditions) 
-		# in place
-
-	return conditions
+from simBehave.misc import process_prng
 
 
-def event_random(N,k,mult=1):
-	"""
-	Simulates (by Monte Carlo methods) a event-related trialset of 
-	N + 1 randomized conditions (adding one to N for the jittered 
-	baseline, which is always 0) of total length N+1*k, where k is 
-	the number of trials/condition.
-	
-	It is assumed that trial and baseline events are of the same length 
-	and that both are interger multilples of the TR.  
+def random(N, k, prng=None):
+    """
+    Creates a trialset of N randomized conditions of total length N+1*k, 
+    where k is the number of trials/condition. Returns a list of conditions.
+    Conditions are indexed from 1; 0 implies terminal conditions, something 
+    this function can't handle.
+    """
 
-	For example, if mult is 1 (default) then each condition event takes 
-	exactly one TR.  If it is 2 then that there are 2 TRs per 
-	condition event, and so on.
+    prng = process_prng(prng)
+    
+    conditions = []
+    [conditions.extend([n, ] * k) for n in range(1, N+1)]
+    prng.shuffle(conditions) 
+        ## in place
 
-	Note: 0 (or null) conditions have a special meaning in the fitRL 
-	functions.
-
-	Returns a list of the conditions; '0' always indicates baseline,
-	the terminal state.
-	"""
-
-	## Create N conditions, randomize their order
-	conditions = []
-	[conditions.extend([int(cond)]*k) for cond in range(0,(N+1))]
-	np.random.shuffle(conditions)
-		# shuffle is inplace, ysy.
-	
-	if mult == 1:
-		return conditions
-	else:
-		conditions_mult = []
-		[conditions_mult.extend([c]*int(mult)) for c in conditions]
-		return conditions_mult
+    return conditions, prng
 
 
-def intra_deter(conditions,n_intra,terminal=True):
-	"""
-	Adds n_intra deterministic intra-trial events to conditions. 
-	If terminal is True, num_intra_event is n+1 events, n plus 
-	a terminal event/state (0).
-	Note: incoming conditions must be integers.
-	"""
-	## 	Each cond in conditions gets expanded by:
-	##		range(cond,(cond*num_intra_event),num_intra_event)
-	
-	## Make tuples for each possible intra.
-	intra_tuples = {}
-	intra_tuples[0] = [0,] * n_intra
-		# Baseline/null states need to 
-		# expanded/mapped to intra_event space.
-	unique_conditions = list(set(conditions))
-	unique_conditions.remove(0)
-	unique_conditions.sort()
-	for uc in unique_conditions:
-		if terminal:
-			intra_tuples[uc] = range(uc,uc+n_intra)+[0,]
-		else:
-			intra_tuples[uc] = range(uc,uc+n_intra)
+def event_random(N, k, mult=1, prng=None):
+    """
+    Simulates (by Monte Carlo methods) a event-related trialset of 
+    N + 1 randomized conditions (adding one to N for the jittered 
+    baseline, which is always 0) of total length N+1*k, where k is 
+    the number of trials/condition.
+    
+    It is assumed that trial and baseline events are of the same length 
+    and that both are interger multilples of the TR.  
 
-	## And map...
-	intra_conditions = []
-	[intra_conditions.extend(intra_tuples[cond]) for cond in conditions]
-	
-	return intra_conditions
+    For example, if mult is 1 (default) then each condition event takes 
+    exactly one TR.  If it is 2 then that there are 2 TRs per 
+    condition event, and so on.
+
+    Note: 0 (or null) conditions have a special meaning in the fitRL 
+    functions.
+
+    Returns a list of the conditions; '0' always indicates baseline,
+    the terminal state.
+    """
+
+    prng = process_prng(prng)
+    
+    ## Create N conditions, randomize their order
+    conditions = []
+    [conditions.extend([int(cond), ] * k) for cond in range(0,(N+1))]
+    prng.shuffle(conditions)
+        ## In place
+    
+    if mult == 1:
+        return conditions, prng
+    else:
+        conditions_mult = []
+        [conditions_mult.extend([c, ] * int(mult)) for c in conditions]
+        return conditions_mult, prng
+
+
+def intra_deter(conditions, n_intra, terminal=True):
+    """
+    Adds n_intra deterministic intra-trial events to conditions. 
+    If terminal is True, num_intra_event is n+1 events, n plus 
+    a terminal event/state (0).
+
+    Note: incoming conditions must be integers.
+    """
+    
+    ## Make tuples for each possible intra.
+    intra_tuples = {}
+    intra_tuples[0] = [0,] * n_intra
+        ## Baseline/null states need to 
+        ## expanded/mapped to intra_event space.
+    
+    unique_conditions = list(set(conditions))
+    unique_conditions.remove(0)
+    unique_conditions.sort()
+    for uc in unique_conditions:
+        if terminal:
+            intra_tuples[uc] = range(uc, uc + n_intra) + [0, ]
+        else:
+            intra_tuples[uc] = range(uc, uc + n_intra)
+
+    ## And map...
+    intra_conditions = []
+    [intra_conditions.extend(intra_tuples[cond]) for cond in conditions]
+    
+    return intra_conditions
